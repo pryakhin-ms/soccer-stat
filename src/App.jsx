2ab10a698f478a0d3842453db91bd4cc6a9248ce
PryakhinMS <pryakhin.ms@gmail.com>
2021-06-24 09:50:31 +0400

    В App подключен ContentPad

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
+import ContentPad from './ContentPad';
 
 export default class App extends Component {
   render() {
     return (
-      <Navbar />
+      <>
+        <Navbar />
+        <ContentPad />
+      </>
     );
   }
 }
