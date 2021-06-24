d53b3e626a5e73f2932e837ed21c83a73ebd8997
PryakhinMS <pryakhin.ms@gmail.com>
2021-06-24 09:53:10 +0400

    Добавлена функция mapStateToProps для подготовки куска состояния для ContentPad. Экспортирован симбиоз mapStateToProps и ContentPad

--- src/ContentPad.jsx
+++ src/ContentPad.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+
+const mapStateToProps = ({ activeTab }) => ({ activeTab });
 
 class ContentPad extends Component {
   render() {
+    const { activeTab } = this.props;
     return (
-      <div className="container" />
+      <div className="container">
+        {activeTab === 'leagues' && <p>LEAGUES</p>}
+        {activeTab === 'teams' && <p>TEAMS</p>}
+      </div>
     );
   }
 }
 
-export default ContentPad;
-// export default connect()(ContentPad);
+export default connect(mapStateToProps)(ContentPad);
