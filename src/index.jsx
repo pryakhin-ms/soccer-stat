d716ec60b65568a43073bac58d45b8d299158a9a
PryakhinMS <pryakhin.ms@gmail.com>
2021-06-24 09:56:23 +0400

    В index.jsx импортирован thunk, createStore, applyMiddleware, reducer. Создано хранилище, к нему применена миддлвара thunk и подключен импортированный редьюсер

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducers';
 import App from './App';
-// import { Provider } from 'react-redux';
-// import thunk from 'redux-thunk';
-// import { createStore, applyMiddleware } from 'redux';
-// import reducer from './reducers';
 
-// const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-/* ReactDOM.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>, document.getElementById('root'),
-); */
+);
