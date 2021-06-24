d400e7446dfde9b409a0425eff43e08320fc6773
PryakhinMS <pryakhin.ms@gmail.com>
2021-06-24 09:09:41 +0400

    Добавлен ContentPad

---
+++ src/ContentPad.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+
+class ContentPad extends Component {
+  render() {
+    return (
+      <div className="container" />
+    );
+  }
+}
+
+export default ContentPad;
+// export default connect()(ContentPad);
